cd7004939f1a0d47a8bf3a8775e74b6285554ec1
rafael <15083988+rafael-ac@users.noreply.github.com>
2024-12-10 22:32:18 -0300

    add hero section

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,6 +3,9 @@ export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
+      colors: {
+        foreground: '#666666',
+      },
       fontFamily: {
         sans: ['Manrope', 'system-ui'],
       },
