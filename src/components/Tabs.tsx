9bdbf201032821949f4345be9e1d44f60fa53e39
rafael <15083988+rafael-ac@users.noreply.github.com>
2025-01-26 21:05:51 -0300

    fix branding items

--- src/components/Tabs.tsx
+++ src/components/Tabs.tsx
@@ -3,13 +3,24 @@ import pd1Apisul from '@assets/pd-1.png';
 import pd2 from '@assets/pd-2.png';
 import pd3 from '@assets/pd-3.png';
 import pd4 from '@assets/pd-4.png';
-import brandingTaller from '@assets/branding/01-branding-taller.png';
-import brandingTechsens from '@assets/branding/02-branding-techsens.png';
-import brandingNoia from '@assets/branding/03-branding-noia.png';
-import brandingRancho from '@assets/branding/04-branding-rancho.png';
-import brandingPousada from '@assets/branding/05-branding-pousada.png';
-import brandingSilva from '@assets/branding/06-branding-silva.png';
-import brandingAmaterasu from '@assets/branding/07-branding-amaterasu.png';
+import brandingTaller from '@assets/branding/taller/home.png';
+import brandingTechsensSquare from '@assets/branding/techsens/home-square.png';
+import brandingTechsensWide from '@assets/branding/techsens/home-wide.png';
+
+import brandingNoiaSquare from '@assets/branding/noia/home-square.png';
+import brandingNoiaWide from '@assets/branding/noia/home-wide.png';
+
+import brandingPousadaSquare from '@assets/branding/pousada/home-square.png';
+import brandingPousadaWide from '@assets/branding/pousada/home-wide.png';
+
+import brandingRanchoSquare from '@assets/branding/rancho/home-square.png';
+import brandingRanchoWide from '@assets/branding/rancho/home-wide.png';
+
+import brandingSilvaSquare from '@assets/branding/silva/home-square.png';
+import brandingSilvaWide from '@assets/branding/silva/home-wide.png';
+
+import brandingAmaterasuSquare from '@assets/branding/amaterasu/home-square.png';
+import brandingAmaterasuWide from '@assets/branding/amaterasu/home-wide.png';
 
 export function Tabs() {
   return (
@@ -38,12 +49,13 @@ export function Tabs() {
       {/* try to wrap the content in a div, change opacity and then animate */}
       {/* https://www.restack.io/p/radix-ui-tabs-animation-answer-cat-ai */}
 
+      {/* PRODUCT DESIGN */}
       <Content
         className="mt-10 space-y-20 data-[state=inactive]:animate-fadeOut data-[state=inactive]:hidden data-[state=active]:animate-fadeIn"
         value="tab1"
       >
-        <a className="space-y-4 block" href="/product-design/apisul">
-          <img src={pd1Apisul.src} alt="" className="w-full" />
+        <a className="space-y-4 block mb-20" href="/product-design/apisul">
+          <img src={pd1Apisul.src} alt="" className="w-full rounded-2xl" />
           <p className="text-blue-gray text-sm font-extralight">Estratégia | UX & UI | Plataforma | B2B</p>
           <p className="text-blue-gray font-light">Grupo Apisul</p>
           <p className="text-cold-gray text-xl font-light">
@@ -52,7 +64,7 @@ export function Tabs() {
         </a>
 
         <a className="space-y-4" href="/product-design/mais-nitido">
-          <img src={pd2.src} alt="" className="w-full" />
+          <img src={pd2.src} alt="" className="w-full rounded-2xl" />
           <p className="text-blue-gray text-sm font-extralight">UX & UI | Plataforma | B2B</p>
           <p className="text-blue-gray font-light">Mais Nítido</p>
           <p className="text-cold-gray text-xl font-light">
@@ -60,69 +72,109 @@ export function Tabs() {
           </p>
         </a>
 
-        <div className="space-y-4">
-          <img src={pd3.src} alt="" className="w-full" />
+        <a className="space-y-4" href="/product-design/dbc">
+          <img src={pd3.src} alt="" className="w-full rounded-2xl" />
           <p className="text-blue-gray text-sm font-extralight">UI | Website Desktop & Mobile</p>
           <p className="text-blue-gray font-light">DBC</p>
           <p className="text-cold-gray text-xl font-light">
             Site para apresentar o novo posicionamento e marca de uma empresa de TI.
           </p>
-        </div>
+        </a>
 
-        <div className="space-y-4">
-          <img src={pd4.src} alt="" className="w-full" />
+        <a className="space-y-4" href="/product-design/apisul-web">
+          <img src={pd4.src} alt="" className="w-full rounded-2xl" />
           <p className="text-blue-gray text-sm font-extralight">UI | Website Desktop & Mobile</p>
           <p className="text-blue-gray font-light">Grupo Apisul</p>
           <p className="text-cold-gray text-xl font-light">Transformando dados em decisões estratégicas.</p>
-        </div>
+        </a>
       </Content>
+
+      {/* BRANDING */}
       <Content
-        className="mt-10 space-y-10 data-[state=inactive]:animate-fadeOut data-[state=inactive]:hidden data-[state=active]:animate-fadeIn"
+        className="mt-10 space-y-20 data-[state=inactive]:animate-fadeOut data-[state=inactive]:hidden data-[state=active]:animate-fadeIn"
         value="tab2"
       >
-        <div className="space-y-4">
-          <img src={brandingTaller.src} alt="" className="" />
+        <a className="block space-y-4" href="/branding/taller">
+          <img src={brandingTaller.src} alt="" className="rounded-2xl" />
           <p className="text-blue-gray text-sm font-extralight">Identidade Visual</p>
           <p className="text-blue-gray font-light">Taller Arquitetura</p>
           <p className="text-cold-gray text-xl font-light">
             Precisão e geometria no design de soluções arquitetônicas.
           </p>
+        </a>
+
+        <div className="flex flex-col gap-20 md:flex-row md:gap-6 md:justify-between">
+          <a className="block space-y-4" href="/branding/techsens">
+            <picture>
+              <source srcSet={brandingTechsensSquare.src} media="(min-width: 768px)" />
+              <source srcSet={brandingTechsensWide.src} media="(max-width: 767px)" />
+              <img src={brandingTechsensWide.src} alt="" />
+            </picture>
+
+            <p className="text-blue-gray text-sm font-extralight">Identidade Visual</p>
+            <p className="text-blue-gray font-light">techsens</p>
+            <p className="text-cold-gray text-xl font-light">Inovação e funcionalidade em automação.</p>
+          </a>
+
+          <a className="block space-y-4" href="/branding/noia-grafica">
+            <picture>
+              <source srcSet={brandingNoiaSquare.src} media="(min-width: 768px)" />
+              <source srcSet={brandingNoiaWide.src} media="(max-width: 767px)" />
+              <img src={brandingNoiaWide.src} alt="" />
+            </picture>
+            <p className="text-blue-gray text-sm font-extralight">Identidade Visual</p>
+            <p className="text-blue-gray font-light">Nóia Gráfica</p>
+            <p className="text-cold-gray text-xl font-light">Conectando arte urbana e movimento independente.</p>
+          </a>
         </div>
-        <div className="space-y-4">
-          <img src={brandingTechsens.src} alt="" className="" />
-          <p className="text-blue-gray text-sm font-extralight">Identidade Visual</p>
-          <p className="text-blue-gray font-light">techsens</p>
-          <p className="text-cold-gray text-xl font-light">Inovação e funcionalidade em automação.</p>
+
+        <div className="flex flex-col gap-20 md:flex-row md:gap-6 md:justify-between">
+          <a className="block space-y-4" href="/branding/rancho-raposo">
+            <picture>
+              <source srcSet={brandingRanchoSquare.src} media="(min-width: 768px)" />
+              <source srcSet={brandingRanchoWide.src} media="(max-width: 767px)" />
+              <img src={brandingRanchoWide.src} alt="" />
+            </picture>
+
+            <p className="text-blue-gray text-sm font-extralight">Identidade Visual</p>
+            <p className="text-blue-gray font-light">Rancho Raposo</p>
+            <p className="text-cold-gray text-xl font-light">Conceito de aconchego e tradição dos ranchos.</p>
+          </a>
+
+          <a className="block space-y-4" href="/branding/pousada-rainha">
+            <picture>
+              <source srcSet={brandingPousadaSquare.src} media="(min-width: 768px)" />
+              <source srcSet={brandingPousadaWide.src} media="(max-width: 767px)" />
+              <img src={brandingPousadaWide.src} alt="" />
+            </picture>
+            <p className="text-blue-gray text-sm font-extralight">Identidade Visual</p>
+            <p className="text-blue-gray font-light">Pousada da Rainha</p>
+            <p className="text-cold-gray text-xl font-light">Design inspirado no aconchego e símbolos marítimos.</p>
+          </a>
         </div>
-        <div className="space-y-4">
-          <img src={brandingNoia.src} alt="" className="" />
-          <p className="text-blue-gray text-sm font-extralight">Identidade Visual</p>
-          <p className="text-blue-gray font-light">Nóia Gráfica</p>
-          <p className="text-cold-gray text-xl font-light">Conectando arte urbana e movimento independente.</p>
-        </div>
-        <div className="space-y-4">
-          <img src={brandingRancho.src} alt="" className="" />
-          <p className="text-blue-gray text-sm font-extralight">Identidade Visual</p>
-          <p className="text-blue-gray font-light">Rancho Raposo</p>
-          <p className="text-cold-gray text-xl font-light">Conceito de aconchego e tradição dos ranchos.</p>
-        </div>
-        <div className="space-y-4">
-          <img src={brandingPousada.src} alt="" className="" />
-          <p className="text-blue-gray text-sm font-extralight">Identidade Visual</p>
-          <p className="text-blue-gray font-light">Pousada da Rainha</p>
-          <p className="text-cold-gray text-xl font-light">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-        </div>
-        <div className="space-y-4">
-          <img src={brandingSilva.src} alt="" className="" />
-          <p className="text-blue-gray text-sm font-extralight">Identidade Visual</p>
-          <p className="text-blue-gray font-light">Silva Schardong</p>
-          <p className="text-cold-gray text-xl font-light">Segurança e estabilidade na construção.</p>
-        </div>
-        <div className="space-y-4">
-          <img src={brandingAmaterasu.src} alt="" className="" />
-          <p className="text-blue-gray text-sm font-extralight">Identidade Visual</p>
-          <p className="text-blue-gray font-light">Amaterasu</p>
-          <p className="text-cold-gray text-xl font-light">Reflexão da elegância e mitologia japonesa no design.</p>
+        <div className="flex flex-col gap-20 md:flex-row md:gap-6 md:justify-between">
+          <a className="block space-y-4" href="/branding/silva-schardong">
+            <picture>
+              <source srcSet={brandingSilvaSquare.src} media="(min-width: 768px)" />
+              <source srcSet={brandingSilvaWide.src} media="(max-width: 767px)" />
+              <img src={brandingSilvaWide.src} alt="" />
+            </picture>
+            <p className="text-blue-gray text-sm font-extralight">Identidade Visual</p>
+            <p className="text-blue-gray font-light">Silva Schardong</p>
+            <p className="text-cold-gray text-xl font-light">Segurança e estabilidade na construção.</p>
+          </a>
+
+          <a className="block space-y-4" href="/branding/amaterasu">
+            <picture>
+              <source srcSet={brandingAmaterasuSquare.src} media="(min-width: 768px)" />
+              <source srcSet={brandingAmaterasuWide.src} media="(max-width: 767px)" />
+              <img src={brandingAmaterasuWide.src} alt="" />
+            </picture>
+
+            <p className="text-blue-gray text-sm font-extralight">Identidade Visual</p>
+            <p className="text-blue-gray font-light">Amaterasu</p>
+            <p className="text-cold-gray text-xl font-light">Reflexão da elegância e mitologia japonesa no design.</p>
+          </a>
         </div>
       </Content>
     </Root>
